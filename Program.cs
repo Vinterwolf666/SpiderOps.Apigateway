42ea9e07238dee1450ca7db0b173bda68a546f57
Vinterwolf666 <vinterwolf666@gmail.com>
2025-03-19 00:18:19 -0500

    Version1.0

---
+++ Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Net;
+using System.Net.Http;
+using Yarp.ReverseProxy.Configuration;
+using Yarp.ReverseProxy.Forwarder;
+
+namespace APIGateway
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+
+            builder.Services.AddReverseProxy()
+    .LoadFromConfig(builder.Configuration.GetSection("ReverseProxy"))
+    .ConfigureHttpClient((context, handler) =>
+    {
+        handler.SslOptions.RemoteCertificateValidationCallback = (sender, cert, chain, sslPolicyErrors) => true;
+    });
+
+
+           
+            builder.Services.AddControllers();
+            builder.Services.AddEndpointsApiExplorer();
+            builder.Services.AddSwaggerGen();
+
+            var app = builder.Build();
+
+            
+            if (app.Environment.IsDevelopment())
+            {
+                app.UseSwagger();
+                app.UseSwaggerUI();
+            }
+
+            
+
+            app.UseRouting();
+
+            
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapReverseProxy(); 
+                endpoints.MapControllers();  
+            });
+
+            app.UseAuthorization(); 
+
+            app.Run();
+        }
+    }
+}
